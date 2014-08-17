d88ae9156778c21f08480bf9b65920a35b109151
Logan Franken <loganfranken@gmail.com>
2014-08-16 18:58:54 -0700

    Completed basic user interface

--- script/UserInterface.js
+++ script/UserInterface.js
@@ -20,12 +20,20 @@ UserInterface.prototype.draw = function(context) {
   context.fillStyle = "rgba(255, 255, 255, 0.1)";
   context.fillRect(1000, 100, 10, 50);
 
+  context.fillStyle = "#FFF";
+  context.font = "18px Arial, sans-serif";
+  context.fillText("0", 1180, 140);
+
   // Draw WATER tile selection
   context.fillStyle = "rgba(33, 104, 196, 1)";
   context.fillRect(1000, 150, 200, 50);
 
   context.fillStyle = "rgba(255, 255, 255, 0.1)";
   context.fillRect(1000, 150, 10, 50);
+
+  context.fillStyle = "#FFF";
+  context.font = "18px Arial, sans-serif";
+  context.fillText("0", 1180, 190);
 
   // Draw FIRE tile selection
   context.fillStyle = "rgba(214, 30, 30, 1)";
@@ -34,11 +42,35 @@ UserInterface.prototype.draw = function(context) {
   context.fillStyle = "rgba(255, 255, 255, 0.1)";
   context.fillRect(1000, 200, 10, 50);
 
+  context.fillStyle = "#FFF";
+  context.font = "18px Arial, sans-serif";
+  context.fillText("0", 1180, 240);
+
   // Draw WIND tile selection
   context.fillStyle = "rgba(112, 208, 230, 1)";
   context.fillRect(1000, 250, 200, 50);
 
   context.fillStyle = "rgba(255, 255, 255, 0.1)";
   context.fillRect(1000, 250, 10, 50);
+
+  context.fillStyle = "#FFF";
+  context.font = "18px Arial, sans-serif";
+  context.fillText("0", 1180, 290);
+
+  // Draw level display
+  context.fillStyle = "#FFF";
+  context.font = "15px Arial, sans-serif";
+  context.fillText("level 0", 1020, 340);
+
+  // Draw reset button
+  context.fillStyle = "rgba(255, 255, 255, 0.1)";
+  context.fillRect(1000, 450, 200, 50);
+
+  context.fillStyle = "rgba(255, 255, 255, 0.1)";
+  context.fillRect(1000, 450, 10, 50);
+
+  context.fillStyle = "#FFF";
+  context.font = "18px Arial, sans-serif";
+  context.fillText("reset", 1030, 480);
 
 };
