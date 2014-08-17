45d963181fda3c034b563b64a7a5e63549b29d69
Logan Franken <loganfranken@gmail.com>
2014-08-16 18:05:32 -0700

    Added Grunt task for minifying

---
+++ Gruntfile.js
@@ -0,0 +1,22 @@
+module.exports = function(grunt) {
+
+  grunt.initConfig({
+
+    pkg: grunt.file.readJSON('package.json'),
+
+    // Minify JS
+    uglify: {
+      minify: {
+        files: {
+          'script/elum.min.js': ['script/Block.js', 'script/Grid.js', 'script/Game.js', 'script/main.js']
+        }
+      }
+    },
+
+  });
+
+  grunt.loadNpmTasks('grunt-contrib-uglify');
+
+  grunt.registerTask('default', ['uglify']);
+
+};
