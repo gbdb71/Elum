d6cf6d346021b6a9e79fae0fb2513ea161901a6f
Logan Franken <loganfranken@gmail.com>
2014-09-07 16:52:11 -0700

    Added comments to the Utility class

--- script/Utility.js
+++ script/Utility.js
@@ -1,9 +1,22 @@
 var UTILITY = {
 
+  /**
+   * Returns a fill style string with the given RGB and opacity values
+   * @param {string}  rgb     - RGB string (format: "RRR, GGG, BBB")
+   * @param {number}  opacity - Opacity of fill style (range: 0 - 1)
+   */
   getRgbaFillStyle: function(rgb, opacity) {
     return "rgba(" + rgb + ", " + opacity + ")";
   },
 
+  /**
+   * Renders a rounded square
+   * @param {CanvasRenderingContext2D}  context       - 2D rendering context to use
+   * @param {integer}                   x             - X-coordinate of square (in pixels)
+   * @param {integer}                   y             - Y-coordinate of square (in pixels)
+   * @param {integer}                   borderRadius  - Border radius of square (in pixels)
+   * @param {integer}                   width         - Width of the square (in pixels)
+   */
   drawRoundedSquare: function(context, x, y, borderRadius, width) {
 
     // Credit: https://developer.mozilla.org/en-US/docs/Web/Guide/HTML/Canvas_tutorial/Drawing_shapes
